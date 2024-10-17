3448944a07376fe176ec59bbd78068c62693baed
Javier Rubio Roca <jrubio@laberit.com>
2024-10-17 09:26:53 +0200

    Project start

---
+++ src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/AwesomeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.laberit.sina.bootcamp.extra.awesomefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/AwesomeFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laberit.sina.bootcamp.extra.awesomefinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
