76da9c712e93e08f924477718f5d201a718f15a2
Javirub <jrubio@laberit.com>
2024-10-19 23:20:16 +0200

    Change PasswordDTO annotation to @Data

--- src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/model/dtos/PasswordDTO.java
+++ src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/model/dtos/PasswordDTO.java
@@ -1,11 +1,9 @@
 package com.laberit.sina.bootcamp.extra.awesomefinalproject.model.dtos;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class PasswordDTO {
     @Size(min = 6, message = "Password must be at least 6 characters long")
     String password;
