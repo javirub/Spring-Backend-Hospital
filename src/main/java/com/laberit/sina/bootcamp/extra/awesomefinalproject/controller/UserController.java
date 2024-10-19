e21f0df696d1cf6d82e45ff6cb8514f8287365bf
Javirub <jrubio@laberit.com>
2024-10-19 21:12:41 +0200

    Finished User personal settings with better logic separation from controller to service

--- src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/controller/UserController.java
+++ src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/controller/UserController.java
@@ -1,46 +1,39 @@
 package com.laberit.sina.bootcamp.extra.awesomefinalproject.controller;
 
-import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.User;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.dtos.PasswordDTO;
-import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.dtos.UserDTO;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
+/**
+ * Controller for user account management.
+ * Allows own users change their password, and delete their account.
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
-
-    @PostMapping("/register")
-    @ResponseBody
-    public User registerPost(@RequestBody @Valid UserDTO userDTO) {
-        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-        return userService.registerUser(userDTO);
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
 
     @PutMapping("/change_password")
     @ResponseBody
-    public ResponseEntity<?> changePassword(@RequestBody @Valid PasswordDTO passwordDTO) {
-        if (passwordDTO.getPassword().equals(passwordDTO.getConfirmPassword())) {
-            passwordDTO.setPassword(passwordEncoder.encode(passwordDTO.getPassword()));
-            boolean isChanged = userService.changePassword(passwordDTO.getPassword());
-            if (isChanged) {
-                return ResponseEntity.ok().build();
-            } else {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Password change failed");
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Passwords do not match");
-        }
+    public ResponseEntity<String> changePassword(@RequestBody @Valid PasswordDTO passwordDTO, Principal principal) {
+        String username = principal.getName();
+        return userService.changePassword(passwordDTO, username);
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public ResponseEntity<String> deleteUser(Principal principal) {
+        String username = principal.getName();
+        return userService.deleteUser(username);
     }
 }
