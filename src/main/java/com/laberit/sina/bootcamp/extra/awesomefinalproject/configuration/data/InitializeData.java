be6ad98c3c9104ff05c40f71ce6a00e9cd301619
Javirub <jrubio@laberit.com>
2024-10-20 11:55:43 +0200

    Initialize patients data

--- src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/configuration/data/InitializeData.java
+++ src/main/java/com/laberit/sina/bootcamp/extra/awesomefinalproject/configuration/data/InitializeData.java
@@ -1,9 +1,12 @@
 package com.laberit.sina.bootcamp.extra.awesomefinalproject.configuration.data;
 
+import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.Patient;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.Role;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.User;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.dtos.UserDTO;
+import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.enums.Gender;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.model.enums.RoleName;
+import com.laberit.sina.bootcamp.extra.awesomefinalproject.repository.PatientRepository;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.repository.RoleRepository;
 import com.laberit.sina.bootcamp.extra.awesomefinalproject.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -11,19 +14,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 @Component
 public class InitializeData implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final PatientRepository patientRepository;
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
 
-    public InitializeData(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+    public InitializeData(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                          RoleRepository roleRepository, PatientRepository patientRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.roleRepository = roleRepository;
+        this.patientRepository = patientRepository;
     }
 
     @Override
@@ -31,6 +38,7 @@ public class InitializeData implements CommandLineRunner {
     public void run(String... args) {
         initializeRoles();
         initializeUsers();
+        initializePatients();
     }
 
     private void initializeUsers() {
@@ -59,6 +67,17 @@ public class InitializeData implements CommandLineRunner {
                     new Role(RoleName.DOCTOR),
                     new Role(RoleName.MANAGER)
             ));
+        }
+    }
+
+    private void initializePatients() {
+        if (patientRepository.count() == 0) {
+            Patient patient = new Patient();
+            patient.setName("Dora");
+            patient.setSurnames("La Exploradora");
+            patient.setBirthDate(LocalDate.of(1993, 8, 14));
+            patient.setGender(Gender.FEMALE);
+            patientRepository.save(patient);
         }
     }
 }
